4422bbe849ddad7e440158594a1966657c4dfc79
Koookadooo <fordriveraustin906@gmail.com>
2023-10-20 15:02:39 -0700

    completed word client

---
+++ wordclient.py
@@ -0,0 +1,98 @@
+import sys
+import socket
+
+# How many bytes is the word length?
+WORD_LEN_SIZE = 2
+
+
+def usage():
+    print("usage: wordclient.py server port", file=sys.stderr)
+
+
+packet_buffer = b''
+
+def get_next_word_packet(s):
+    """
+    Return the next word packet from the stream.
+
+    The word packet consists of the encoded word length followed by the
+    UTF-8-encoded word.
+
+    Returns None if there are no more words, i.e. the server has hung
+    up.
+    """
+
+    global packet_buffer
+
+    while True:
+        # Check if buffer starts with a word length size
+        if len(packet_buffer) >= WORD_LEN_SIZE:
+            # Extract the word length
+            word_len = int.from_bytes(packet_buffer[:WORD_LEN_SIZE], byteorder='big')
+            
+            # Check if the buffer contains the entire word
+            if len(packet_buffer) >= WORD_LEN_SIZE + word_len:
+                word_packet = packet_buffer[WORD_LEN_SIZE:WORD_LEN_SIZE + word_len]
+                # Strip the packet data off the front of the buffer
+                packet_buffer = packet_buffer[WORD_LEN_SIZE + word_len:]
+                # Return the packet data
+                return word_packet
+
+        # Receive more data
+        data = s.recv(5)
+
+        # If amount of data received is zero bytes, return connection closed indicator
+        if not data:
+            return None
+
+        # Append received data onto the buffer
+        packet_buffer += data
+
+
+def extract_word(word_packet):
+    """
+    Extract a word from a word packet.
+
+    word_packet: a word packet consisting of the encoded word length
+    followed by the UTF-8 word.
+
+    Returns the word decoded as a string.
+    """
+
+    # Extract the encoded word length
+    word_len = int.from_bytes(word_packet[:WORD_LEN_SIZE], byteorder='big')
+
+    # Extract the UTF-8 word using the length
+    word = word_packet[:WORD_LEN_SIZE+word_len].decode()
+
+    return word
+
+# Do not modify:
+def main(argv):
+    try:
+        host = argv[1]
+        port = int(argv[2])
+    except Exception:
+        usage()
+        return 1
+
+    s = socket.socket()
+    s.connect((host, port))
+
+    print("Getting words:")
+
+    while True:
+        word_packet = get_next_word_packet(s)
+
+        if word_packet is None:
+            break
+
+        word = extract_word(word_packet)
+
+        print(f"    {word}")
+
+    s.close()
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
